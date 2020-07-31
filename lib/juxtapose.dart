b673e8204d6330efc9bd3fc12700f02a94c94c88
Leslie Arkorful <lesliearkorful@gmail.com>
2020-07-31 04:26:08 +0000

    Update juxtapose.dart

--- lib/juxtapose.dart
+++ lib/juxtapose.dart
@@ -1,3 +1,7 @@
+/// A widget for comparing two stacked widgets by dragging a slider thumb to
+/// reveal either sides of the slider.
+///
+/// Sliding direction can be horizontal or vertical.
 library juxtapose;
 
 export 'src/juxtapose_widget.dart';
